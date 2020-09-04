15afade6b9a9e4a3d8fca63063ffa8952e645e4f
Karthik SK <41192803+karthiksk-ef@users.noreply.github.com>
2020-09-04 11:35:48 +0530

    Adding empty dev.js file

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID: '',
+    googleClientSecret: '',
+    mongoURI: '',
+    cookieKey: '',
+    stripePublishableKey: '',
+    stripeSecretKey: '',
+    sendGridKey: '',
+    redirectDomain: 'http://localhost:3000',
+    facebookClientID: '',
+    facebookClientSecret: ''
+};
